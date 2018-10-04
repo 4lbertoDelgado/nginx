fd4cdafe5695bafc3f59ad6e42deb2c4cc6cd939
4lbertoDelgado <albertodq7@gmail.com>
2018-10-04 10:34:36 -0500

    Update vartest.lua

--- files/me/src/lua/vartest.lua
+++ files/me/src/lua/vartest.lua
@@ -1,6 +1,6 @@
+-- Asignamos los valores de variables de nginx a variables locales lua
 local ngx_var = ngx.var.var_thing;
 local ngx_undef = ngx.var.undefined_var;
-local ngx_var2 = ngx.var.var_thing2;
 
 ngx.say("var_thing is type: " .. type(ngx_var))
 ngx.say("var_thing contains: " .. tostring(ngx_var))
@@ -8,5 +8,3 @@ ngx.say("var_thing contains: " .. tostring(ngx_var))
 ngx.say("undefined_var is type: " .. type(ngx_undef))
 ngx.say("undefined_var contains: " .. tostring(ngx_undef))
 
-ngx.say("var_thing2 is type: " .. type(ngx_var2))
-ngx.say("var_thing2 contains: " .. tostring(ngx_var2))
